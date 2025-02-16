f772aa6c1a43a753ebbc7145626f43dd741a6411
Hafidz Al Arsyad <hafidzala@gmail.com>
2025-02-17 06:11:15 +0700

    App - rescue record not found with jsonapi errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found(exception)
+    render jsonapi_errors: [{status: "404", title: "Not Found", detail: exception.message }], status: :not_found
+  end
 end
