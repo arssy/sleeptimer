bd68bf270609e88cb6e795d5995950bdfc1b579c
Hafidz Al Arsyad <hafidzala@gmail.com>
2025-02-17 14:05:52 +0700

    User - fix followers and followings relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_many :sleep_histories, dependent: :destroy
   
   has_many :followers, class_name: "Follower", foreign_key: :following_id, dependent: :destroy
-  has_many :user_followers, through: :followers, source: :following
+  has_many :user_followers, through: :followers, source: :user
 
   has_many :followings, class_name: "Follower", foreign_key: :user_id, dependent: :destroy
-  has_many :user_followings, through: :followings, source: :user
+  has_many :user_followings, through: :followings, source: :following
 
   def fall_asleep?
     sleep_histories.active.exists?
